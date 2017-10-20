06d24244c591eb974c4148f09813ce834374b295
Loïc JIMENEZ <nier57@hotmail.fr>
2017-10-20 15:41:38 -0400

    ajout de la classe VehileLeader

--- TP2/VehicleLeader.h
+++ TP2/VehicleLeader.h
@@ -2,16 +2,46 @@
 #define VEHICLECHASER_H
 #pragma once
 
+
 #include "Vehicle.h"
+#include "2d/Vector2D.h"
+#include "misc/Smoother.h"
+
+#include <vector>
+#include <list>
+#include <string>
+
+class GameWorld;
+class SteeringBehavior;
 
 
 
-
-class VehicleLeader //: public Vehicle
+class VehicleLeader : public Vehicle
 {
+private: 
+
+	//define if the vehicle is controlled or not
+	bool		m_isManuallyControled;
+
+
 public:
-	VehicleLeader();
+
+	VehicleLeader(GameWorld* world,
+		Vector2D position,
+		double    rotation,
+		Vector2D velocity,
+		double    mass,
+		double    max_force,
+		double    max_speed,
+		double    max_turn_rate,
+		double    scale,
+		bool isManuallyControled);
+
 	~VehicleLeader();
+
+	//-------------------------------------------accessor methods
+	bool Controlled() { return m_isManuallyControled; }
+
 };
 
 #endif
